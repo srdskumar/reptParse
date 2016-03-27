bffd19597a34d3dca26f72452c8ffa2fe04df568
srdskumar <suresh.isrd@gmail.com>
2016-03-27 21:37:12 +0530

    Added files via upload
    DB Code Added

--- SimpleExcelReaderExample.java
+++ SimpleExcelReaderExample.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.*;
  
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -22,15 +23,99 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 public class SimpleExcelReaderExample {
      
     public static void main(String[] args) throws IOException {
+        
+        ArrayList<String> open = parseReport(5,1,2,5,8,44,46,"open");
+        ArrayList<String> close = parseReport(24,1,2,5,8,44,46,"close");
+        cleanDB();
+        //importDB(open);
+        
+        Connection conn = null;
+        Statement stmt = null;
+        String sql = "";
+                 
+        try {
+            
+        Connection con = DBConnection.getConnection();
+        stmt = con.createStatement();
+        
+        Iterator<String> itr=open.iterator();  
+        while(itr.hasNext()){  
+        sql = itr.next();
+        stmt.executeUpdate(sql);
+        System.out.println(sql); 
+        }
+        
+        itr=close.iterator();  
+        while(itr.hasNext()){  
+        sql = itr.next();
+        stmt.executeUpdate(sql);
+        System.out.println(sql); 
+        }
+            
+            
+        }
+        catch (SQLException se){
+            se.printStackTrace();
+        }
+        
+        
+        Iterator<String> itr=open.iterator();  
+        while(itr.hasNext()){  
+        System.out.println(itr.next());  
+        }  
+        
+        itr=close.iterator();  
+        while(itr.hasNext()){  
+        System.out.println(itr.next());  
+        }  
+        
+    }
+    
+    public static void cleanDB()
+    {
+        
+        Connection conn = null;
+        Statement stmt = null;
+        String sql = "";
+                 
+        try {
+            
+        Connection con = DBConnection.getConnection();
+        stmt = con.createStatement();
+        stmt.executeUpdate("delete from open");      
+        stmt.executeUpdate("delete from close");
+            
+            
+        }
+        catch (SQLException se){
+            se.printStackTrace();
+        }
+        
+        
+    }
+ 
+    public static ArrayList<String> parseReport(int sno,int c1,int c2,int c3,int c4,int c5,int c6,String tbl) throws IOException
+    {
+        
+                    
         String excelFilePath = "Books.xlsx";
         FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
-         
+        System.out.print("Parsing Sheet Number ---> "+sno);
+        System.out.println();
+        ArrayList<String> ar = new ArrayList<String>();
+        String sqlstr = "";
+        
         Workbook workbook = new XSSFWorkbook(inputStream);
-        Sheet uninv_open = workbook.getSheetAt(5);
+        Sheet uninv_open = workbook.getSheetAt(sno);
         Iterator<Row> iterator = uninv_open.iterator();
          
                 String rec = "";
@@ -39,6 +124,8 @@ public class SimpleExcelReaderExample {
                 String svc = "";
                 double dval = 0;
                 double cval = 0;
+                String rpps = "";
+                
                 
         while (iterator.hasNext()) {
             Row nextRow = iterator.next();
@@ -47,43 +134,40 @@ public class SimpleExcelReaderExample {
              cval=0;
             while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
-                if(cell.getColumnIndex() == 1 || cell.getColumnIndex() == 2 || cell.getColumnIndex() == 5 || cell.getColumnIndex() == 8 || cell.getColumnIndex() == 45 || cell.getColumnIndex() == 46)  
+                if(cell.getColumnIndex() == c1 || cell.getColumnIndex() == c2 || cell.getColumnIndex() == c3 || cell.getColumnIndex() == c4 || cell.getColumnIndex() == c5 || cell.getColumnIndex() == c6)  
                 {
-                
-                  
                         
                 switch (cell.getCellType()) {
                     case Cell.CELL_TYPE_STRING:
                         
-                        if(cell.getColumnIndex() == 1)
+                        if(cell.getColumnIndex() == c1)
                         {
                             rec=cell.getStringCellValue();
                         }
-                        if (cell.getColumnIndex() == 2 )
+                        if (cell.getColumnIndex() == c2 )
                         {
                             pay=cell.getStringCellValue();
                         }
-                        if (cell.getColumnIndex() == 5 )
+                        if (cell.getColumnIndex() == c3 )
                         {
                             svc=cell.getStringCellValue();
                         }
-                         if (cell.getColumnIndex() == 8 )
+                         if (cell.getColumnIndex() == c4 )
                         {
                             per=cell.getStringCellValue();
                         }
-                         
-                                                 
+                                                                        
                         break;
                     case Cell.CELL_TYPE_BOOLEAN:
                         //System.out.print(cell.getBooleanCellValue());
                         break;
                     case Cell.CELL_TYPE_NUMERIC:
                         //System.out.print(cell.getNumericCellValue());
-                        if (cell.getColumnIndex() == 45 )
+                        if (cell.getColumnIndex() == c5 )
                         {
                             dval=cell.getNumericCellValue();
                         }
-                        if (cell.getColumnIndex() == 46 )
+                        if (cell.getColumnIndex() == c6 )
                         {
                             cval=cell.getNumericCellValue();
                         }
@@ -93,14 +177,27 @@ public class SimpleExcelReaderExample {
                 }
                 
             }
-            System.out.print(rec+"-"+pay+"-"+per+"-"+svc+"|"+dval+"|"+cval);
-            System.out.println();
+            if(rec.length() == 5){
+            rpps = rec+"-"+pay+"-"+per+"-"+svc;
+            //System.out.print(rpps+"|"+dval+"|"+cval);
+            //System.out.println("insert into "+tbl+" (rpps,sdrval) values(\""+rpps+"\","+dval+")");
+            //System.out.println();
+            // ADD insert Query to Array 
+            sqlstr = "insert into "+tbl+" (rpps,sdrval) values(\""+rpps+"\","+dval+")";
+            ar.add(sqlstr);
+            //stmt.executeUpdate("insert into "+tbl+" (rpps,sdrval) values(\""+rpps+"\","+dval+")");
+            }
         }
          
         workbook.close();
         inputStream.close();
+        
+        return ar;
+        
     }
- 
+    
+    
+    
 }
 
 
